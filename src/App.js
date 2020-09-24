f17c7641a447e2ad6f3980f702d7c28b922569d3
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-09-24 20:26:11 +0530

    container final

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="container text-center py-5 mx-auto align-middle">
-        <div className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">React + TailwindCSS</div>
+      <div className="container text-center py-5 mx-auto align-middle bg-teal-600">
+        <div className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">React + TailwindCSS</div>
       </div>
     );
   }
