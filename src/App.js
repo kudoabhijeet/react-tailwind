b6f54d0ea316d1208fee35601198eb63b54f5ff5
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-09-24 20:24:25 +0530

    Tailwind with PostCSS configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1> Hello World </h1>
+      <div className="container text-center py-5 mx-auto align-middle">
+        <div className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">React + TailwindCSS</div>
       </div>
     );
   }
